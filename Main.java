828461ddf8cbd3c560531fa65017c835f97eb040
jjennyvc <153659964+jjennyvc@users.noreply.github.com>
2025-01-17 19:04:08 +0000

    lol

--- Main.java
+++ Main.java
@@ -5,10 +5,10 @@ public class Main
   public static void main(String[] args)
   {
     // Problem 1 - Create a scanner and an array of size 3
-
+      double [] scanner = [3];
     // Problem 2
     // Use the scanner to get user input and give it to the method
-
+      
     int N = /* scan.nextInt() */;
     int[] output = sumsOfNums(N);
   }
